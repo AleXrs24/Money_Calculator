447bc5fb6089cc80bc3e9bacb74b9818aa20bbc8
AleXrs24 <AleXrs24@AleXrs24>
2015-01-18 00:30:13 +0000

    commit

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,30 +1,3 @@
-/*package application;
-
-//import console.ExchangeDisplay;
-import control.ExchangeOperation;
-//import control.SwingExchangeOperation;
-import mock.CurrencySetLoader;
-import model.CurrencySet;
-//import swing.ApplicationFrame;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-
-public class Application {
-
-    public static void main(String[] args) {
-        /*CurrencySet currencySet = new CurrencySetLoader().load();
-        final ApplicationFrame frame = new ApplicationFrame(currencySet.toArray());
-        frame.register(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new SwingExchangeOperation(frame.getExchangeDialog(), new ExchangeDisplay()).execute();
-            }
-        });
-        CurrencySet currencySet = new CurrencySetLoader().load();
-        ExchangeOperation operation = new ExchangeOperation(currencySet);
-        operation.execute();
-    }
-}*/  
 package application;
 
 import swing.MoneyDisplay;
